764f664d9816e670887058f8baf6a8595b2b7bb5
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-18 04:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   {
   if(string === whisper(string))
-    return console.log("I can't hear you!")
-  elseif(string === shout(string))
+  { return console.log("I can't hear you!")
+  }
+  elseif(string === shout(string)){
     return console.log("YES INDEED!")
-  elseif(string === "I love you, Grandma.")
+  }
+  elseif(string === "I love you, Grandma."){
     return console.log("I love you too.")
+  }
   }
 }
