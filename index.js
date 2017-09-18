58554ff17cc8c23bc1c45629fa8c3abe10e01f24
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-18 02:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if(string === shout(string)
-    return logShout("yes indeed") 
+    return logShout("yes indeed")
   elseif (string === whisper(string)
     return logWhisper("I can't hear you!")
   elseif (string = "I love you, Grandma.")
